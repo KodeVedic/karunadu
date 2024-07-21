d2b5ac894a4cdb731592650df5b77f0c3c633f5b
swatisaulanki <swatisaulanki@gmail.com>
2024-07-21 19:55:28 +0530

    code refactor

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const items = document.querySelectorAll(".timeline-item");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    items.forEach(item => {
+        observer.observe(item);
+    });
+});
